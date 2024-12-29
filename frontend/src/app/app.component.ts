7e93da79ac6a30ce384163a01c49994fe0d112c6
Jhon Harold Hincapie <jhonmaster+git@gmail.com>
2024-12-29 14:59:00 -0500

    Agregar componente de pie de página al layout principal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ``,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
